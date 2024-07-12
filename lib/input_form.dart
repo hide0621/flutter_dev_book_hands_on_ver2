477c81c6c2fb812797687bc2aed4893c73461cf2
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-07-12 09:09:35 +0900

    テキスト入力フォームにバリデーション機能を追加

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -8,30 +8,68 @@ class InputForm extends StatefulWidget {
 }
 
 class _InputFormState extends State<InputForm> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        /// Paddingウィジェット
-        /// TextFieldウィジェットが横一杯に広がらないように余白を設ける
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
+    // return Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   children: [
+    //     /// Paddingウィジェット
+    //     /// TextFieldウィジェットが横一杯に広がらないように余白を設ける
+    //     Padding(
+    //       padding: const EdgeInsets.symmetric(horizontal: 16),
+    //       child: TextField(
+    //           maxLines: 5,
+    //           decoration: const InputDecoration(
+    //             hintText: '文章を入力してください',
+    //           )),
+    //     ),
+    //
+    //     /// SizedBoxウィジェット
+    //     /// TextFieldウィジェットとElevatedButtonウィジェットの間に余白を設ける
+    //     const SizedBox(height: 20),
+    //     ElevatedButton(
+    //       onPressed: () {},
+    //       child: const Text('変換'),
+    //     )
+    //   ],
+    // );
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          /// Paddingウィジェット
+          /// TextFieldウィジェットが横一杯に広がらないように余白を設ける
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextFormField(
               maxLines: 5,
               decoration: const InputDecoration(
                 hintText: '文章を入力してください',
-              )),
-        ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '文章が入力されていません';
+                }
+                return null;
+              },
+            ),
+          ),
 
-        /// SizedBoxウィジェット
-        /// TextFieldウィジェットとElevatedButtonウィジェットの間に余白を設ける
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('変換'),
-        )
-      ],
+          /// SizedBoxウィジェット
+          /// TextFieldウィジェットとElevatedButtonウィジェットの間に余白を設ける
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              final formState = _formKey.currentState!;
+              formState.validate();
+            },
+            child: const Text('変換'),
+          )
+        ],
+      ),
     );
   }
 }
