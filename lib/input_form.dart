9203be30c8e63340bd73962dc60ee3c81f406679
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-07-12 09:11:10 +0900

    不要なコードを削除

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -12,29 +12,6 @@ class _InputFormState extends State<InputForm> {
 
   @override
   Widget build(BuildContext context) {
-    // return Column(
-    //   mainAxisAlignment: MainAxisAlignment.center,
-    //   children: [
-    //     /// Paddingウィジェット
-    //     /// TextFieldウィジェットが横一杯に広がらないように余白を設ける
-    //     Padding(
-    //       padding: const EdgeInsets.symmetric(horizontal: 16),
-    //       child: TextField(
-    //           maxLines: 5,
-    //           decoration: const InputDecoration(
-    //             hintText: '文章を入力してください',
-    //           )),
-    //     ),
-    //
-    //     /// SizedBoxウィジェット
-    //     /// TextFieldウィジェットとElevatedButtonウィジェットの間に余白を設ける
-    //     const SizedBox(height: 20),
-    //     ElevatedButton(
-    //       onPressed: () {},
-    //       child: const Text('変換'),
-    //     )
-    //   ],
-    // );
     return Form(
       key: _formKey,
       child: Column(
